ac925d372381faa217bd08d5442cb39c5b15d7f0
Kelechi Orunta <kelechiorunta1@gmail.com>
2026-03-31 11:02:59 +0100

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+        <button
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </button>
+      </section>
+
+      <div className="ticks">{ count}</div>
+    </>
+  )
+}
+
+export default App
