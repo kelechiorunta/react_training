848eb31db2c4afbded3f598e32237bcad9d54bb0
Kelechi Orunta <kelechiorunta1@gmail.com>
2026-04-12 23:35:23 +0100

    feat: add chatRoutes

--- server/app.ts
+++ server/app.ts
@@ -1,5 +1,8 @@
 import express, { type Express } from "express";
+import chatRouter from "./routes/chatsRouter";
 
 const app: Express = express();
+
+app.use("/message", chatRouter);
 
 export default app;
