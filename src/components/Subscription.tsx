2f662c8ef7496cd967278ecef8dcc24eff5a2564
Kelechi Orunta <kelechiorunta1@gmail.com>
2026-04-12 23:43:33 +0100

    feat: pass down props to Subscription

--- src/components/Subscription.tsx
+++ src/components/Subscription.tsx
@@ -1,19 +1,51 @@
-import { useEffect, useEffectEvent } from "react";
+import {
+  // useEffect,
+  // useEffectEvent,
+  useRef,
+  useState,
+  type Dispatch,
+  type SetStateAction,
+} from "react";
 
-export default function Subscription() {
-  const onMessage = useEffectEvent((event: MessageEvent) => {
-    console.log("Message From Server", event.data);
-  });
+export type SubscriptionType = {
+  socketServerRef: ReturnType<typeof useRef<WebSocket>>;
+  chats: ReturnType<typeof useState<{ message: string; from: string }[]>>[0];
+  setChats: Dispatch<SetStateAction<object>>;
+  handleSubmit: (e: React.SubmitEvent<HTMLFormElement>) => void;
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  formdata: { message: string };
+};
+export default function Subscription({
+  chats,
+  handleSubmit,
+  handleChange,
+  formdata,
+}: SubscriptionType) {
+  return (
+    <div>
+      <h2>Subscription Chat</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          name="message"
+          value={formdata.message}
+          onChange={handleChange}
+        />
+        <input type="submit" value={"Submit"} />
+      </form>
 
-  useEffect(() => {
-    const socket = new WebSocket("ws://localhost:3500");
-    socket.onmessage = (event: MessageEvent) => {
-      onMessage(event);
-      socket.send("Hi from Client");
-    };
-
-    return () => socket.close();
-  }, []);
-
-  return <div>Subscription</div>;
+      <section style={{ padding: 4 }}>
+        {chats?.map((chat) => (
+          <p
+            style={
+              chat.from === "server"
+                ? { justifySelf: "left" }
+                : { justifySelf: "right" }
+            }
+          >
+            {chat.message}
+          </p>
+        ))}
+      </section>
+    </div>
+  );
 }
