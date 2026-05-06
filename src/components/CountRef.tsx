708869a6ab36fc5ede6a232f2d576da813456390
Kelechi Orunta <kelechiorunta1@gmail.com>
2026-05-06 11:43:24 +0100

    add notes and caveats for useRef

--- src/components/CountRef.tsx
+++ src/components/CountRef.tsx
@@ -1,5 +1,16 @@
 import React, { useCallback, useEffect, useRef, useState } from "react";
 
+//Caveats
+// By using a ref, you ensure that:
+
+// 1. You can store information between re-renders (unlike regular local variables, which reset on every render).
+// 2. Changing it does not trigger a re-render (unlike state variables, which trigger a re-render).
+
+//Note:
+//Changing a ref does not trigger a re-render,
+//so refs are not appropriate for storing information
+//you want to display on the screen.
+//Use state for that instead.
 export default function CountRef() {
   const [count, setCount] = useState(0);
   const [user, setUser] = useState({ name: "Tega", role: "developer" });
@@ -12,7 +23,7 @@ export default function CountRef() {
   }, []);
 
   useEffect(() => {
-    setUser((prevUser) => ({ ...prevUser, name: "Tochi" }));
+    // setUser((prevUser) => ({ ...prevUser, name: "Tochi" }));
     // const countId = setInterval(() => {
     // countRef.current = countRef.current + 1;
     // }, 1000);
@@ -23,8 +34,8 @@ export default function CountRef() {
   const handleCounter = () => {
     // x = x + 1;
     countRef.current = countRef.current + 1;
-    setCount((prevCount) => prevCount + 1);
-    console.log("CountRef", countRef.current);
+    // setCount((prevCount) => prevCount + 1);
+    alert(`CountRef is ${countRef.current}`);
   };
 
   return (
@@ -35,7 +46,7 @@ export default function CountRef() {
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
       />
-      <h1>Render countRef: {count}</h1>
+      <h1>Render countRef: {countRef.current}</h1>
       <button onClick={handleCounter}>Add Count</button>
     </>
   );
