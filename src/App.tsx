bab81ca2a53c077be88a61ca6e1a3b15c8d6e778
unknown <tegaguru@gmail.com>
2026-05-06 13:46:01 +0100

    assessment for use ref completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
 // import { useState } from 'react'
 
 import "./App.css";
+// import Subscription from "./components/Subscription";
+// import NormalizedStateComponent from "./components/NormalizedStateComponent";
+// import TaskOne from "./components/zekeri/useEffect/TaskOne";
+// import TaskOneA from "./components/zekeri/useEffect/TaskOneA";
+// import Task from "./components/zekeri/useEffect/Task";
+// import Students from "./components/zekeri/useState/Task1";
+// import ClassTimerComponent from "./components/zekeri/useEffect/ClassTimerComponent";
+// import FunctionalTimerComponent from "./components/FunctionalTimerComponent";
+// import MouseTracker from "./components/tega/useEffect/MouseTracker";
+// import FunctionalTimerComponent from "./components/FunctionalTimerComponent";
+// import OptimizedTimerComponent from "./components/OptimizedTimerComponent";
+// import UnOptimizedTimerComponent from "./components/UnOptimizedTimerComponent";
+// import TaskTwo from "./components/zekeri/useEffect/TaskTwo";
 import {
   useRef,
   useState,
@@ -10,9 +23,12 @@ import {
   useEffect,
   useCallback,
 } from "react";
-import Footballer from "./components/Footballer";
-import CountRef from "./components/CountRef";
-import StopWatch from "./components/StopWatch";
+import UserProfileForm from "./components/tega/Assessment/Assessment";
+import InputRef from "./components/InputRef";
+// import Footballer from "./components/Footballer";
+// import UserProfileForm from "./components/tega/Assessment/Assessment";
+
+
 // import CacheFunctionComponent from "./components/CacheFunctionComponent";
 
 function App() {
@@ -24,10 +40,10 @@ function App() {
   ]);
   const [digit, setDigit] = useState(0);
 
-  // const onSSEConnection = useEffectEvent(() => {
-  //   const sseSource = new EventSource("/sse/message/sse/data");
-  //   sourceSSERef.current = sseSource;
-  // });
+  const onSSEConnection = useEffectEvent(() => {
+    const sseSource = new EventSource("/sse/message/sse/data");
+    sourceSSERef.current = sseSource;
+  });
 
   const onWebSocketConnection = useEffectEvent(() => {
     const socket = new WebSocket("http://localhost:5174/websocket");
@@ -52,7 +68,7 @@ function App() {
 
   useEffect(() => {
     onWebSocketConnection();
-    // onSSEConnection();
+    onSSEConnection();
   }, []);
 
   useEffect(() => {
@@ -109,11 +125,8 @@ function App() {
   const [footballers, setFootballers] = useState<{ name: string }[]>([]);
 
   const handleAddFootballer = () => {
-    setFootballers((prev) => [...prev, footballer]);
-    console.log([...footballers, footballer]);
+    console.log(footballers);
   };
-
-  const cacheFn = useCallback(handleAddFootballer, [footballer.name]);
 
   // const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
   //   const { value } = e.target;
@@ -122,8 +135,66 @@ function App() {
 
   return (
     <>
-      <CountRef />
-      <StopWatch />
+    <UserProfileForm/>
+    <InputRef/>
+      {/* <input
+        name="footballer"
+        value={footballer.name}
+        onChange={(e) =>
+          setFootballer((prev) => ({ ...prev, name: e.target.value }))
+        }
+      ></input>
+      <Footballer
+        footballers={footballers}
+        handleAddFootballer={handleAddFootballer}
+      /> */}
+      {/* <section id="center">
+        <button
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </button>
+      </section>
+
+      <div className="ticks">{ count}</div> */}
+      {/* <ClassTimerComponent>
+        <h3>ClassTimerComponent</h3>
+      </ClassTimerComponent> */}
+      {/* <FunctionalTimerComponent /> */}
+      {/* <MouseTracker /> */}
+      {/* <OptimizedTimerComponent /> */}
+      {/* <UnOptimizedTimerComponent /> */}
+      {/* <TaskTwo /> */}
+      {/* <Subscription
+        handleChange={
+          handleChange as (e: React.ChangeEvent<HTMLInputElement>) => {}
+        }
+        handleSubmit={
+          handleSubmit as (e: React.SubmitEvent<HTMLFormElement>) => {}
+        }
+        formdata={formdata as { message: string }}
+        socketServerRef={
+          socketServerRef as ReturnType<typeof useRef<WebSocket>>
+        }
+        chats={
+          chats as ReturnType<
+            typeof useState<{ message: string; from: string }[]>
+          >[0]
+        }
+        setChats={setChats as Dispatch<SetStateAction<object>>}
+      />
+      <CacheFunctionComponent
+        digit={digit as number}
+        handleAdd={handleAdd as () => void}
+      /> */}
+
+      {/* <NormalizedStateComponent /> */}
+
+      <br />
+      {/* <Students /> */}
+
+      {/* <TaskOneA /> */}
     </>
   );
 }
--- src/components/tega/Assessment/AssessmentII.md
+++ src/components/tega/Assessment/AssessmentII.md
@@ -9,25 +9,38 @@ Managing timers and mutable values without re-renders
 
 🧩 Section A: Conceptual Questions (Short Answer)
 What is the purpose of the useRef hook in React?
+persist data that is not rendered
+
 What does the .current property represent?
+it's an object property that contains the current value of useref
+
 How is useRef different from useState?
+useref values do not cause a re render unlike useState
+
 Does updating a useRef value trigger a re-render? Why or why not?
+no it does not. because it does not trigger re-render like useState. React can't track the current property.
+
 When should you use useRef instead of useState?
+referencing dom elements
+storing timer ids from javascript setTimeut object and setInterval object
 
 🧠 Section B: Code Understanding
 Question 6
 
 What will happen when the button is clicked?
 
+it will increase the value of the countRef.current by 1
 const countRef = useRef(0);
 
 const handleClick = () => {
-  countRef.current += 1;
-  console.log(countRef.current);
+countRef.current += 1;
+console.log(countRef.current);
 };
 Will the UI update?
+no
 Why?
-
+it does not trigger a re-render like useState
+because react cannot track the current property of the ref object to render a ui update
 Question 7
 
 Explain what this code does:
@@ -35,7 +48,7 @@ Explain what this code does:
 const inputRef = useRef<HTMLInputElement | null>(null);
 
 useEffect(() => {
-  inputRef.current?.focus();
+inputRef.current?.focus();
 }, []);
 
-
+we're referencing a dom element to activate it's focus state by calling the focus method
